99a5ee0bde8a87b1c96983df45a65d14563aadec
Aditya <repackergame@gmail.com>
2021-09-15 05:14:34 +0530

    implement env

--- src/includes/firebase/firebaseApp.ts
+++ src/includes/firebase/firebaseApp.ts
@@ -1,14 +1,20 @@
 import { initializeApp } from "firebase/app";
+import {config} from "../../../env"
+let firebaseConfig = {};
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB2YHXy72oRtSSINmc2dZJl0pGs4z4skp8",
-  authDomain: "music-53f73.firebaseapp.com",
-  projectId: "music-53f73",
-  storageBucket: "music-53f73.appspot.com",
-  messagingSenderId: "999447624230",
-  appId: "1:999447624230:web:a9897cb1736f8178ef04e7",
-  measurementId: "G-VHVMGX964Y"
-};
+if(process.env.VUE_APP_VERCEL_ENV === "production"){
+  firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  }
+}else{
+  firebaseConfig = config
+}
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
